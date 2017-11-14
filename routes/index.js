d76b83ab7de296e65eb79f9719aa0b8bd77ae0e9
Dimitri Tolios <dtolios@riotgames.com>
2017-11-14 13:52:37 -0800

    added all books route and adjusted home pug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('home', { title: 'Library Manager' });
+});
+
+/* GET page */
+router.get('/all-books', function(req, res, next) {
+    res.render('all_books', { title: 'Books' });
 });
 
 module.exports = router;
