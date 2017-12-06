02148f0f2bc76b47c3f8309fb47086ff716f5267
Dimitri Tolios <dtolios@riotgames.com>
2017-12-06 10:19:09 -0800

    Separated controller logic from the router

---
+++ controllers/bookController.js
@@ -0,0 +1,117 @@
+/* eslint-disable comma-dangle,prefer-destructuring */
+const winston = require('winston');
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Op = Sequelize.Op;
+
+exports.index = function(req, res) {
+  let title = 'Books';
+  let whereObj = null;
+  delete req.session.updateBookErrors;
+
+  if (req.query.filter === 'overdue') {
+    title = 'Overdue Books';
+    whereObj = [
+      {
+        return_by: {
+          [Op.lt]: new Date(),
+        },
+      },
+      {
+        returned_on: null,
+      },
+    ];
+  }
+  if (req.query.filter === 'checked_out') {
+    title = 'Checked Out Books';
+    whereObj = {
+      returned_on: null,
+    };
+  }
+
+  db.book.findAll({
+    include: [{
+      model: db.loan,
+      where: whereObj,
+    }],
+  })
+  .then((books) => {
+    res.render('books', { books, title });
+  })
+  .catch((error) => {
+    winston.log('error', error);
+    res.sendStatus(500);
+  });
+};
+
+exports.book_create_get = function(req, res) {
+  res.render('books/create', { book: db.book.build(), title: 'New Book' });
+};
+
+exports.book_detail_get = function(req, res) {
+  db.book.findById(
+    req.params.id,
+    {
+      include: [{
+        model: db.loan,
+        include: [{
+          model: db.book,
+        }, {
+          model: db.patron,
+        }],
+      }],
+    }
+  )
+  .then((book) => {
+    if (book) {
+      res.render('books/detail', {
+        book, loans: book.Loans, title: book.title, errors: req.session.updateBookErrors
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  });
+};
+
+exports.book_create_post = function(req, res) {
+  db.book.create(req.body)
+  .then(() => {
+    res.redirect('/books');
+  })
+  .catch((error) => {
+    if (error.name === 'SequelizeValidationError') {
+      res.render('books/create', {
+        book: db.book.build(req.body),
+        title: 'New Book',
+        errors: error.errors,
+      });
+    } else {
+      throw error;
+    }
+  })
+  .catch((error) => {
+    winston.log('error', error);
+    res.sendStatus(500);
+  });
+};
+
+exports.book_detail_put = function(req, res) {
+  db.book.findById(req.params.id)
+  .then((book) => {
+    if (!book) {
+      res.sendStatus(404);
+    }
+    return book.update(req.body);
+  })
+  .then(() => {
+    res.redirect('/books');
+  })
+  .catch((error) => {
+    winston.log('error', error);
+    if (error.name === 'SequelizeValidationError') {
+      req.session.updateBookErrors = error.errors;
+      res.redirect(`/books/${req.params.id}`);
+    }
+  });
+};
--- routes/books.js
+++ routes/books.js
@@ -1,143 +1,38 @@
 /* eslint-disable comma-dangle,prefer-destructuring */
 const express = require('express');
-const winston = require('winston');
-const Sequelize = require('sequelize');
-const db = require('../db');
-
-
 const router = express.Router();
-const Op = Sequelize.Op;
+
+const book_controller = require('../controllers/bookController');
 
 /**
  * GET /books
  * Renders the list of books page
  * If the filter parameter is set, will filter results
  */
-router.get('/', (req, res) => {
-  let title = 'Books';
-  let whereObj = null;
-  delete req.session.updateBookErrors;
-
-  if (req.query.filter === 'overdue') {
-    title = 'Overdue Books';
-    whereObj = [
-      {
-        return_by: {
-          [Op.lt]: new Date(),
-        },
-      },
-      {
-        returned_on: null,
-      },
-    ];
-  }
-  if (req.query.filter === 'checked_out') {
-    title = 'Checked Out Books';
-    whereObj = {
-      returned_on: null,
-    };
-  }
-
-  db.book.findAll({
-    include: [{
-      model: db.loan,
-      where: whereObj,
-    }],
-  })
-    .then((books) => {
-      res.render('books', { books, title });
-    })
-    .catch((error) => {
-      winston.log('error', error);
-      res.sendStatus(500);
-    });
-});
+router.get('/', book_controller.index);
 
 /**
  * GET /books/create
  * Renders the book creation page
  */
-router.get('/create', (req, res) => {
-  res.render('books/create', { book: db.book.build(), title: 'New Book' });
-});
+router.get('/create', book_controller.book_create_get);
 
 /**
  * GET /books/:id
  * Renders the book detail page, which includes the update form
  */
-router.get('/:id', (req, res) => {
-  db.book.findById(
-    req.params.id,
-    {
-      include: [{
-        model: db.loan,
-        include: [{
-          model: db.book,
-        }, {
-          model: db.patron,
-        }],
-      }],
-    }
-  )
-    .then((book) => {
-      if (book) {
-        res.render('books/detail', {
-          book, loans: book.Loans, title: book.title, errors: req.session.updateBookErrors
-        });
-      } else {
-        res.sendStatus(404);
-      }
-    });
-});
+router.get('/:id', book_controller.book_detail_get);
 
 /**
  * POST /books
  * Handler for creating a new book resource
  */
-router.post('/', (req, res) => {
-  db.book.create(req.body)
-    .then(() => {
-      res.redirect('/books');
-    })
-    .catch((error) => {
-      if (error.name === 'SequelizeValidationError') {
-        res.render('books/create', {
-          book: db.book.build(req.body),
-          title: 'New Book',
-          errors: error.errors,
-        });
-      } else {
-        throw error;
-      }
-    })
-    .catch((error) => {
-      winston.log('error', error);
-      res.sendStatus(500);
-    });
-});
+router.post('/', book_controller.book_create_post);
 
 /**
  * PUT /books/:id
  * Handler for updating an existing book resource
  */
-router.put('/:id', (req, res) => {
-  db.book.findById(req.params.id)
-    .then((book) => {
-      if (!book) {
-        res.sendStatus(404);
-      }
-      return book.update(req.body);
-    })
-    .then(() => {
-      res.redirect('/books');
-    })
-    .catch((error) => {
-      winston.log('error', error);
-      if (error.name === 'SequelizeValidationError') {
-        req.session.updateBookErrors = error.errors;
-        res.redirect(`/books/${req.params.id}`);
-      }
-    });
-});
+router.put('/:id', book_controller.book_detail_put);
 
 module.exports = router;
